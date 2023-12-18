db312da2234003496d909976991c61f67b8fb342
Maxim <115576894+GitMaxm@users.noreply.github.com>
2023-12-18 19:24:47 +0300

    Add files via upload

--- start-gulp/gulp-html-scss-js webcademy.ru/src/js/modules/mobile-nav.js
+++ start-gulp/gulp-html-scss-js webcademy.ru/src/js/modules/mobile-nav.js
@@ -4,11 +4,28 @@ function mobileNav() {
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
 
+	// Функция для закрытия меню
+	function closeNav() {
+		nav.classList.remove('mobile-nav--open');
+		menuIcon.classList.remove('nav-icon--active');
+		document.body.classList.remove('no-scroll');
+	}
+
 	navBtn.onclick = function () {
 		nav.classList.toggle('mobile-nav--open');
 		menuIcon.classList.toggle('nav-icon--active');
 		document.body.classList.toggle('no-scroll');
 	};
+
+	// Получаем все ссылки внутри меню
+	const navLinks = document.querySelectorAll('.mobile-nav a');
+
+	// Добавляем обработчик события для каждой ссылки
+	navLinks.forEach(function (link) {
+		link.addEventListener('click', function () {
+			closeNav(); // Закрываем меню при клике на ссылку
+		});
+	});
 }
 
 export default mobileNav;
